59faa2ac41c611b4d957271797fcec017eaab44d
Dane-Dawson <Dane.P.Dawson@gmail.com>
2020-04-14 07:08:48 -0500

    basic input bar interface

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import GameHeader from './GameHeader'
 import GameShow from './GameShow'
 import GameFooter from './GameFooter'
 
+let baseURL = 'http://localhost:3000/'
 class App extends Component {
   constructor(){
     super();
     this.state = {
-
+      level: [],
+      monsters: [],
+      users: [],
+      activeUser: [],
+      words: [],
+      bossWords: [],
+      
     }
   }
   componentDidMount(){
+    this.fetchLevel()
     //capture details of gameplay and user information
   }
+  
+  fetchLevel = () => {
+    return fetch(baseURL + "levels")
+    .then(resp => resp.json())
+    .then(level => this.setState({level}))
+  }
+
+  
 
   render() {
     return(
       <div className="App">
       <GameHeader />
-      <GameShow />
+      <GameShow words={this.state.level}/>
       <GameFooter />
     </div>
     )
--- src/GameFooter.js
+++ src/GameFooter.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class GameFooter extends Component {
-    render() {
-        return(
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center'
-            }}>
-                <h3>Game Footer</h3>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = { value: "", words: this.props.words };
+
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  checkValue = (event) => {
+      const index = this.state.words.indexOf(event.target.value)
+      if (index > -1) {
+          console.log(event.target.value)
+        this.setState(this.state.words.splice(index, 1));
+        this.setState({value: ''})
+        event.target.value = ''
+      }
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ value: event.target.value });
+    this.checkValue(event)
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <form>
+          <input type="text" name="name" onChange={this.handleChange} />
+        </form>
+        <h3>Game Footer</h3>
+      </div>
+    );
+  }
 }
 export default GameFooter;
