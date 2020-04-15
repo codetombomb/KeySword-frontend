dabdbc05255c572835ff8e343fc816e21fb38bb5
Dane-Dawson <Dane.P.Dawson@gmail.com>
2020-04-14 21:41:47 -0500

    minor tweaking and styling

--- src/UserBar.js
+++ src/UserBar.js
@@ -60,10 +60,11 @@ class UserBar extends Component {
             }}
           >
             <form onSubmit={this.logInUser}>
-              <label>Username</label>
+              <label>Username: </label>
               <input type="text" name="username" />
               <br></br>
-              <label>Password</label>
+              <br></br>
+              <label>Password: </label>
               <input type="password" name="password" />
               <br></br>
               <button >submit</button>
@@ -91,10 +92,11 @@ class UserBar extends Component {
             }}
           >
             <form onSubmit={this.createUser}>
-              <label>Username</label>
+              <label>Username: </label>
               <input type="text" name="username" />
               <br></br>
-              <label>Password</label>
+              <br></br>
+              <label>Password: </label>
               <input type="password" name="password" />
               <br></br>
               <button>submit</button>
